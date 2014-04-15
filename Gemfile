87873c7565d75a7415f48bf5e9acf2b4ca5c91dc
Ashley Rae Lee & Jason Mannon <student@epicodus.com>
2014-04-15 09:57:29 -0700

    Initial commit, user log in and sign up completed

---
+++ Gemfile
@@ -0,0 +1,29 @@
+source 'https://rubygems.org'
+
+gem 'rails'
+gem 'pg'
+gem 'sass-rails'
+gem 'uglifier'
+gem 'coffee-rails'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'bcrypt'
+gem 'bootstrap-sass'
+gem 'bootstrap-sass-extras'
+
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'quiet_assets'
+end
+
+group :test, :development do
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'factory_girl'
+end
+
+group :test do
+  gem 'shoulda-matchers'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TwitterClone::Application.config.secret_key_base = '3d89b7ecaf9bebba048da82fa3ad0ef72d9b6f5f448405f67923d5511ae2e1ef5c4be43f81835d61d865f60999c1ee5e9001a797390a728eca3fdc18601cfaaa'
